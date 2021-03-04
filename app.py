ea384bfff3a098f23ba55aa2b70179ac6b2a59b9
Lauren Parrish <ljeanparrish@gmail.com>
2021-03-04 17:34:44 -0500

    updated html code

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ Base.classes.keys()
 
 # Save reference to the table
 measurement = Base.classes.measurement
-station = Base.classes.station
+Station = Base.classes.station
 
 ##############################################################################
 # Flask Setup
@@ -60,7 +60,7 @@ def precipitation():
 
      results = session.query(measurement.date, measurement.prcp).all()
      
-     session.close()
+     #session.close()
 
      # Create a dictionary from the row data and append to a list of all_percipitation
      all_percipitation = []
@@ -72,10 +72,7 @@ def precipitation():
          
      return jsonify(all_percipitation)
 
-    #######################2nd Option##########################################################
-
-    ##PROBLEM - CANNOT GET BOTH STATION AND NAME TO RETURN.  CODE DOES NOT LIKE STATION
-
+    #################################################################################
 @app.route("/api/v1.0/stations")
 def stations():
     # Create a session to link from Python to the DB
@@ -83,9 +80,9 @@ def stations():
     
     """Return a list of stations"""
     
-    stations = session.query(measurement.station).distinct()
+    stations = session.query(measurement.station, Station.name).distinct()
     
-    session.close()
+    #session.close()
 
     # Create a dictionary from the station data and append to a list of all_stations
     all_stations = []
@@ -95,54 +92,6 @@ def stations():
          all_stations.append(station_dict)
     
     return jsonify(all_stations)
-
-    #################OPTION 1 NOT WORKING################################################################
-  
-    #result2 = session.query(station.station, station.name).all()
-    #result2 = session.query(station, station.station, station.name).all()
-    
-    # session.close()
-    
-    # # Create a dictionary from the station data and append to a list of all_stations
-    # all_stations = []
-    # for station, name in result2:
-    #     station_dict = {}
-    #     station_dict["station"] = station
-    #     station_dict["name"] = name
-    #     all_stations.append(station_dict)
-    
-    # return jsonify(all_stations)
-
-#########################Option 3 Not Working###########################################################
-
-# Define what to do when a user hits the /stations route
-
-# SELECT
-# measurement.station,
-# station.name,
-# FROM measurement
-# INNER JOIN station ON
-# measurement.station = station.station;
-
-# @app.route("/api/v1.0/stations")
-# def stations():
-#     # Create a session to link from Python to the DB
-#     session = Session(engine)
-    
-#     """Return a list of stations"""
-   
-#     stations = session.query(measurement.station, measurement.name).distinct()
-#     session.close()
-
-#     # Create a dictionary from the station data and append to a list of all_stations
-#      all_stations = []
-#     for station in stations:
-#          station_dict = {}
-#          station_dict["station"] = station
-#          station_dict["name"] = name
-#          all_stations.append(station_dict)
-    
-#     return jsonify(all_stations)
 
 ################################################################################################################ 
     
@@ -178,34 +127,23 @@ def tobs():
 
 ############################HOW TO DO ALIGATOR CLIPS##############################
 
-#api/v1.0/<start> and /api/v1.0/<start>/<end>
-
-#Return a JSON list of the minimum temperature, the average temperature,and  the max temperature 
-# for a given start or start-end range.
-
-#When given the start only, calculate TMIN, TAVG, and TMAX, for all dates greater than and equal 
-# to the start date.
-
-#When given the start and the end date, calculate the TMIN, TAVG, and TMAX for dates between the 
-# start and end date inclusive.
-
-  # Create a session to link from Python to the DB
-     session = Session(engine)
-
-     """Return a  list of the minimum temperature, the average temperature,.\
-        and the max temperature for a given start or start-end range"""
+@app.route("/api/v1.0/<start>")
+def temperature_s(start):
+    # Set start and end dates for date range
+    start_date = '2016-08-23'
+    end_date = '2017-08-23'
+    
+    """Return a  list of min_temp, avg_temp, & max_temp for a given start or start-end range"""
 
     ##Query from database full temp results over a range of dates
     temp_results = session.query(measurement.date, measurement.tobs).all()
      
-    temp_details = [measurement.date,
-                    measurement.tobs]
+    temp_details = [measurement.date,measurement.tobs]
 
     temp_range = session.query(*temp_details).\
                     filter(measurement.date >= '2016-08-23').\
                     filter(measurement.date <= '2017-08-23').all()
-    session.close()
-    
+        
     ##Find the min, max, avg temperature in that date range
     temp_range_min = session.query(temp_range.tobs, func.min(temp_range.tobs)).first()
 
@@ -216,6 +154,22 @@ def tobs():
     return jsonify(temp_range_min)
     return jsonify(temp_range_max)
     return jsonify(temp_range_avg)
+
+@app.route("/api/v1.0/<start>/<end>")
+def temperature(start, end):
+    start_date = '2016-08-23'
+    end_date = '2017-08-23'
+    
+    def calc_temps(start_date, end_date):
+        
+        return session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start_date).filter(Measurement.date <= end_date).all()
+        
+        trip_temps = calc_temps(start_date, end_date)
+        
+        return jsonify(trip_temps)
+        
+        session.close()
 
 if __name__ == "__main__":
      app.run(debug=True)
